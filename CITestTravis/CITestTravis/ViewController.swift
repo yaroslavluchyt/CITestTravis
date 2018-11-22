7f90027a938fe8ec9a88739c40bbe91a23c22a83
Yaroslav Luchyt <yluch@softserveinc.com>
2018-11-22 16:31:29 +0200

    signed commit 2

--- CITestTravis/CITestTravis/ViewController.swift
+++ CITestTravis/CITestTravis/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    lazy var variable = 10
+    lazy var variable = 100
 
     override func viewDidLoad() {
         super.viewDidLoad()
